259f11665b730fa345631c992a67be2ba73f4a40
Dmitry ILiyushkó <79217526217@ya.ru>
2020-07-16 18:39:58 +0300

    Delete usingsys.py

--- usingsys.py
+++
@@ -1,8 +0,0 @@
-#-*-coding:utf-8*-
-#!/usr/bin/env python
-''''''
-import sys
-print('Аргументы командной строки:')
-for i in sys.argv:
-    print(i)
-print('\n\nПеременная PYTHONPATH содержит', sys.path, '\n')
