b0fd4467397b49ad9d2ff1140ae76be183de6cdb
Dmitry ILiyushkó <79217526217@ya.ru>
2020-07-16 18:25:49 +0300

    Delete definitions2.py

--- definitions2.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-'
-
-from __future__ import absolute_import
-import os
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
-CONFIG_PATH = os.path.join(ROOT_DIR, u'Agent.conf')  # 
